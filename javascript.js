02424c370347d02022c38b4c6c877fe21992518a
FaustasL <faustas.labanauskas@gmail.com>
2017-07-02 21:11:12 +0300

    All files
    asd

---
+++ javascript.js
@@ -0,0 +1,28 @@
+function active(){
+  var searchBar = document.getElementById('searchBar');
+
+  if(searchBar.value == 'Search...'){
+    searchBar.value = ''
+    searchBar.placeholder= 'Search...'
+  }
+}
+function inactive(){
+  var searchBar = document.getElementById('searchBar');
+
+  if(searchBar.value == ''){
+    searchBar.value = 'Search...'
+    searchBar.placeholder= ''
+  }
+}
+
+$(document).ready(function(){
+  $('#btn').click(function(e){
+
+    var linkHref = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(linkHref).offset().top
+    })
+
+    e.preventDefault();
+  })
+})
