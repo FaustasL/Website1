8d31657b0f0e23450d62f9f5018bfb48f4a7bc0e
CukrausPudra <august.macijauskas@gmail.com>
2017-07-09 22:07:46 +0300

    Paziurek categories dali

--- Once_more/once_more.js
+++ Once_more/once_more.js
@@ -1,4 +1,30 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+//smoothScroll
+$(document).ready(function(){
+  $('#cmon').click(function(e){
+
+    var linkHref = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(linkHref).offset().top
+    })
+
+    e.preventDefault();
+  })
+})
+
+//smoothScroll
+$(document).ready(function(){
+  $('#btn').click(function(e){
+
+    var linkHref = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(linkHref).offset().top
+    })
+
+    e.preventDefault();
+  })
+})
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon*/
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
